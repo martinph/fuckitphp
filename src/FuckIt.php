b1f11a689b40e79905a2675e1d86dcea6ca83c1b
Martin Hughes <martinph01@gmail.com>
2015-10-02 00:21:37 +0100

    Fuck it

---
+++ src/FuckIt.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace FuckIt;
+
+/**
+ * Class FuckIt
+ * @package FuckIt
+ */
+class FuckIt
+{
+
+    /**
+     * @var object
+     */
+    protected $fuckedObject;
+
+    /**
+     * @var \ReflectionObject
+     */
+    protected $fuckedObjectReflection;
+
+    /**
+     * @param object $object
+     */
+    public function __construct($object)
+    {
+        if(!is_object($object)) {
+            $object = new \stdClass();
+        }
+        $this->fuckedObject = $object;
+        $this->fuckedObjectReflection = new \ReflectionObject($object);
+    }
+
+    /**
+     * @param string $method
+     * @param array $arguments
+     * @return $this
+     */
+    public function __call($method, $arguments)
+    {
+        try {
+            if($this->hasMethod($method)) {
+                $result = call_user_func_array([$this->fuckedObject, $method], $arguments);
+                $class = get_class($this->fuckedObject);
+                if($result instanceof $class) {
+                    return $this;
+                }
+                return $result;
+            }
+        } catch(\Exception $e) {
+            // Fuck it
+        }
+    }
+
+    /**
+     * @param string $property
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        try {
+            if($this->hasProperty($property)) {
+                return $this->fuckedObject->$property;
+            }
+        } catch(\Exception $e) {
+            // Fuck this too
+        }
+    }
+
+    /**
+     * @param string $property
+     * @param mixed $value
+     */
+    public function __set($property, $value)
+    {
+        try {
+            if($this->hasProperty($property)) {
+                $this->fuckedObject->$property = $value;
+            }
+        } catch(\Exception $e) {
+            // This can get fucked as well
+        }
+    }
+
+    /**
+     * @param string $property
+     * @return bool
+     */
+    public function __isset($property)
+    {
+        try {
+            if($this->hasProperty($property)) {
+                return isset($this->fuckedObject->$property);
+            }
+        } catch(\Exception $e) {
+            // You want some? Well fuck off
+        }
+        return false; // Fuck it
+    }
+
+    /**
+     * @param string $property
+     */
+    public function __unset($property)
+    {
+        try {
+            if($this->hasProperty($property)) {
+                unset($this->fuckedObject->$property);
+            }
+        } catch(\Exception $e) {
+            // Not this time, fucker
+        }
+    }
+
+    /**
+     * @param string $property
+     * @return bool
+     */
+    protected function hasProperty($property)
+    {
+        return $this->fuckedObjectReflection->hasProperty($property);
+    }
+
+    /**
+     * @param string $method
+     * @return bool
+     */
+    protected function hasMethod($method)
+    {
+        return $this->fuckedObjectReflection->hasMethod($method);
+    }
+}
---
+++ src/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FuckIt;
+
+if(!function_exists("FuckIt\fuckit")) {
+
+    /**
+     * @param object $object
+     * @return FuckIt
+     */
+    function fuckit($object) {
+        return new FuckIt($object);
+    }
+
+}
